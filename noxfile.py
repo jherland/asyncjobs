e955388c81c3039224927510e48011159d1f192d
Johan Herland <johan@herland.net>
2020-07-14 12:11:30 +0200

    noxfile.py: Allow passing extra command-line arguments to pytest

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ nox.options.keywords = "not dist"
 @nox.session(python=["3.6", "3.7", "3.8", "3.9"])
 def tests(session):
     session.install(".[test]")
-    session.run("pytest", "-x", "--log-level=debug")
+    session.run("pytest", "-x", "--log-level=debug", *session.posargs)
 
 
 @nox.session
