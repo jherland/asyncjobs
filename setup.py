1be027cab39f2ad3451766135100be7fe07b9386
Johan Herland <johan@herland.net>
2020-08-04 03:12:00 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="asyncjobs",
-    version="0.5.0",
+    version="0.5.5",
     description="Asynchronous job scheduler",
     long_description=long_description,
     long_description_content_type="text/markdown",
